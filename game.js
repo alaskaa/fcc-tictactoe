b728fcd2d93824de3f0e444c217b406b224ab890
Sibylle Sehl <sibylle.sehl@googlemail.com>
2018-02-07 22:19:13 +0000

    some initial functionality and attempts at minimax (currently exceeding)

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ var aiPlayer = "";
 var selected = false;
 
 
+// the human player selects his choice of symbol
 function setPlayerChoice(symbol) {
   if(symbol == "X") {
     humanPlayer = "X";
@@ -20,13 +21,14 @@ function setPlayerChoice(symbol) {
 }
 
 
+// calculate the free Spaces left on the board that haven't been taken up yet
 function calculateFreeSpaces() {
   return board.filter(s => s != "O" && s != "X");
 }
 
 // function that returns a boolean whether a wining state has been reached
 // takes in a board and a symbol that corressponds to the player
-function winStateReached(board, symbol) {
+function winStateReached(symbol) {
 
   if (
     (board[0] == symbol && board[4] == symbol && board[8] == symbol) ||
@@ -48,41 +50,56 @@ function winStateReached(board, symbol) {
 
 
 // AI Player is the maximiser and humanPlayer is the minimiser
-function calcMiniMax(board, depth, isMaximizingPlayer) {
+function calcMiniMax(board, isMaximizingPlayer, symbol) { // depth
 
   // get the free board spaces that are not taken up so far
-  var freeSpaces = calculateFreeSpaces(board);
+  //var freeSpaces = calculateFreeSpaces(); // board
 
-  //if(current board state is a terminal state) {
-  //  return value of the board;
-  // }
+  if(winStateReached(symbol) === true) {
+    console.log("You win!");
+  }
 
+  else {
     // returns the best move for the players
 
     if(isMaximizingPlayer === true) { // so aiPlayer is playing
         var bestValue = -1000;        // set to very low number
-        for(var move of freeSpaces) {
-            var value = calcMiniMax(freeSpaces, depth+1, false);
-            bestValue = Math.max(bestValue, value);
+        for(var move of board) {
+            var value_arr = [];
+            value_arr.push(calcMiniMax(board, false, symbol)); //depth+1, false);
+
         }
+        bestValue = Math.max(... value_arr);
         return bestValue;
 
 
     } else {    // so humanPlayer is playing
         var bestValue = +1000;
-        for(var move in freeSpaces) {
-            var value = calcMiniMax(freeSpaces, depth+1, true);
-            bestValue = Math.min(bestValue, value);
+        for(var move in board) {
+            var value_arr = [];
+            value_arr.push(calcMiniMax(board, true, symbol));
+
         }
+        bestValue = Math.min(... value_arr);
         return bestValue;
     }
+  }
 
 }
 
-function findBestMove(board) {
+function findBestMove(board, aiPlayer) {
+  var bestValue = -1000;
+  var bestMove;
   for(var cell in board) {
-    var bestMove = minimax(); // continue here
+    var someMoveValue = calcMiniMax(board, true, aiPlayer); // continue here
+    if(someMoveValue > bestValue) {
+      bestValue = someMoveValue;
+      bestMove = cell;
+    }
+   board[bestMove] = aiPlayer;
   }
+
+
 }
 
 
@@ -96,46 +113,64 @@ document.getElementById("one").onclick = () => {
   var p = document.getElementById("one").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[0] = humanPlayer;
+  console.log(board);
 }
 document.getElementById("two").onclick = () => {
   var p = document.getElementById("two").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[1] = humanPlayer;
+  console.log(board);
 }
 document.getElementById("three").onclick = () => {
   var p = document.getElementById("three").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[2] = humanPlayer;
+  console.log(board);
 }
 document.getElementById("four").onclick = () => {
   var p = document.getElementById("four").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[3] = humanPlayer;
+  console.log(board);
 }
 document.getElementById("five").onclick = () => {
   var p = document.getElementById("five").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[4] = humanPlayer;
+  console.log(board);
 }
 document.getElementById("six").onclick = () => {
   var p = document.getElementById("six").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[5] = humanPlayer;
+  console.log(board);
 }
 document.getElementById("seven").onclick = () => {
   var p = document.getElementById("seven").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[6] = humanPlayer;
+  console.log(board);
 }
 document.getElementById("eight").onclick = () => {
   var p = document.getElementById("eight").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[7] = humanPlayer;
+  console.log(board);
 }
 document.getElementById("nine").onclick = () => {
   var p = document.getElementById("nine").childNodes;
   p[0].innerHTML = humanPlayer;
   console.log(p);
+  board[8] = humanPlayer;
+  console.log(board);
 }
 }
 
@@ -159,3 +194,7 @@ document.getElementById("nine").onclick = () => {
      completeSetup();
    }
   };
+
+// Player 1 starts and places something
+
+// Ai player's go
