ee54f8ad3a360ec8a854119e32b1e47a9a7af2b5
Sibylle Sehl <sibylle.sehl@googlemail.com>
2018-02-11 13:01:31 +0000

    fixed winStateReached() vertical state bug

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ function winStateReached(board, symbol) {
     (board[0] == symbol && board[1] == symbol && board[2] == symbol) ||
     (board[3] == symbol && board[4] == symbol && board[5] == symbol) ||
     (board[6] == symbol && board[7] == symbol && board[8] == symbol) ||
-    (board[3] == symbol && board[3] == symbol && board[6] == symbol) ||
+    (board[0] == symbol && board[3] == symbol && board[6] == symbol) ||
     (board[1] == symbol && board[4] == symbol && board[7] == symbol) ||
     (board[2] == symbol && board[5] == symbol && board[8] == symbol)
    ) {
